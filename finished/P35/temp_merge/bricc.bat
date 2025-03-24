c6e38715fb238a3acb6f009a7a06ca792d7f6f64
Lijie Sun <47992432+sunlijie-msu@users.noreply.github.com>
2025-03-23 23:32:36 -0400

    FLAG, Allowed β for J in Adopted

---
+++ finished/P35/temp_merge/bricc.bat
@@ -0,0 +1,6 @@
+cd D:\X\ND\Files\A35\finished\P35\temp_merge
+set BrIccHome=D:\X\ND\Files\A35\finished\P35\temp_merge
+D:\X\ND\Files\A35\finished\P35\temp_merge\BrIcc.exe D:\X\ND\Files\A35\finished\P35\new\P35_adopted.tmp<D:\X\ND\Files\A35\finished\P35\temp_merge\bricc_pars.txt
+D:\X\ND\Files\A35\finished\P35\temp_merge\BrIcc.exe D:\X\ND\Files\A35\finished\P35\new\P35_adopted.tmp MERGE<D:\X\ND\Files\A35\finished\P35\temp_merge\bricc_merge_pars.txt
+echo done>D:\X\ND\Files\A35\finished\P35\temp_merge\dummy.txt
+exit
